2a5854c4c5dfe3908ec7df77fdbdbc627c4cd2cd
Mayank Srivastav <mastermayank786@gmail.com>
2026-06-28 22:24:14 +0530

    updated the backend url

--- frontend/src/services/taskApi.js
+++ frontend/src/services/taskApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api/tasks";
+const API_URL = "https://task-tracker-backend-wu27.onrender.com/api/tasks";
 
 export const getTasks = () => {
   return axios.get(API_URL);
