912594d9536e33eff81e13c527e8f65cb45c1944
Mayank Srivastav <mastermayank786@gmail.com>
2026-06-28 22:15:32 +0530

    Updated server to use environment port

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,16 @@ require("dotenv").config();
 const app = require("./src/app");
 const connectDB = require("./src/db/db");
 
+
 connectDB();
 
 
+const PORT = process.env.PORT || 3000;
 
 
 
 
 
-
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
     console.log("server is running on port 3000");
 })
